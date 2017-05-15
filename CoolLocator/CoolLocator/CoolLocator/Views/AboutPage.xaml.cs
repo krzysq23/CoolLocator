337239294be73d61d6249ae1560f791ae2f21f45
KSkarbowski <kskarbowski@inglot.pl>
2017-05-15 15:07:33 +0200

    Create Xamarin.Foirms Portable Project

---
+++ CoolLocator/CoolLocator/CoolLocator/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace CoolLocator.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
